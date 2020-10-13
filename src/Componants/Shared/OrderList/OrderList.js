ecaf9931617cdff2271cbe1ca2774620fcc77c30
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2020-10-13 19:15:30 +0600

    Order List Done

--- src/Componants/Shared/OrderList/OrderList.js
+++ src/Componants/Shared/OrderList/OrderList.js
@@ -8,8 +8,14 @@ const OrderList = () => {
             <div className="col-md-5 OrderListDetails">
                 <div className="orderImg">
                     <img src={order} alt=""/>
+                    <button className="btn btn-dark statusBtn">Status</button>
+                </div>
+                <div className="orderText ">
+                    <h3>Web & Mobile design</h3>
+                    <p>We craft stunning and amazing web UI, using a well drrafted UX to fit your product.</p>
                 </div>
             </div>
+            
         </div>
     );
 };
