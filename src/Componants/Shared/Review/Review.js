1672ba579d5416763b80a1b4e5793afe6b51c628
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2020-10-14 22:32:06 +0600

    comments part added for sarver

--- src/Componants/Shared/Review/Review.js
+++ src/Componants/Shared/Review/Review.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from '../../../App';
 import OrderList from '../OrderList/OrderList';
 import UserSettingMenu from '../UserSettingMenu/UserSettingMenu';
 
 const Review = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const addComments = () => {
+        let CompanyDesignation = document.getElementById('CompanyDesignation').value;
+        let reviewMessage = document.getElementById('reviewMessage').value;
+        let emailAddressForComment = document.getElementById('emailAddressForComment').value;
+
+        let commentsInfo = {designation: CompanyDesignation,message: reviewMessage,email: emailAddressForComment};
+        
+        console.log(commentsInfo);
+
+        fetch('http://localhost:5000/addComments',{
+            method: 'POST',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify(commentsInfo)
+        })
+        .then(response => response.json())
+        .then(success => {})
+    }
+
     return (
         <div className="container review">
             <div className="row">
@@ -19,10 +39,10 @@ const Review = () => {
                 <div className = "row">
             <div className="col-md-12">
                 <div className="orderImg footerRight">
-                <form action="">
-                        <input type="email" placeholder="Your email address" name="" id="" required />
-                        <input type="text" name="" id="" placeholder="Company’s name, Designation" required />
-                        <textarea placeholder="Your message" name="" id="" cols="30" rows="5" required></textarea>
+                <form onSubmit = {addComments}>
+                        <input id="emailAddressForComment" type="email" placeholder="Your email address" name="" required />
+                        <input type="text" name="" id="CompanyDesignation" placeholder="Company’s name, Designation" required />
+                        <textarea placeholder="Your message" name="" id="reviewMessage" cols="30" rows="5" required></textarea>
                         <input className="btn btn-dark footerSendBtn" type="submit" value="send" />
                     </form>
                 </div>                
